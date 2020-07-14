beeefa8d219f94f9c873e608eee06cdec254de4b
Sasaki1994 <b.92421933411@gmail.com>
2020-07-14 11:45:50 +0000

    20200714

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from Collector import Collector
 
 def collect_stock():
     collector = Collector()
-    try:
-        collector.collect_daily_data()
-    except:
-        try:
-            collector.collect_daily_data()
-        except:
-            collector.collect_daily_data()
+    collector.collect_daily_data()
 
 
 if __name__ == '__main__':
